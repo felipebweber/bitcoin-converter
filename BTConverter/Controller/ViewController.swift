76f7be2270b6ecf1cd90c5a4dd3659ae2c00db2a
Felipe Weber <weberecomp@gmail.com>
2020-05-15 21:31:54 -0300

    Add components

--- BTConverter/Controller/ViewController.swift
+++ BTConverter/Controller/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var labelCoin: UILabel!
+    @IBOutlet weak var labelValue: UILabel!
+    @IBOutlet weak var currentPicker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        currentPicker.delegate = self
+        currentPicker.dataSource = self
     }
-
 
 }
 
+extension ViewController: UIPickerViewDataSource {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 4
+    }
+    
+}
+
+extension ViewController: UIPickerViewDelegate {
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        print("Teste")
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "Teste"
+    }
+}
