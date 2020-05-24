b3da913d0f220c1f537d92e7d387fda6c06dea36
popcoding <pop@popcoding.com>
2020-05-23 17:25:13 -0700

    Removido o CoinManager do AppDelegate. Não é necessário no AppDelegate pois é chamado no viewDidLoad do ViewController

--- bitcoin-converter/AppDelegate.swift
+++ bitcoin-converter/AppDelegate.swift
@@ -14,12 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
-    var coinManager = CoinManager()
-    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        coinManager.fetchCoinPrice()
-        coinManager.retreiveData(currency: "USD")
         
         return true
     }
