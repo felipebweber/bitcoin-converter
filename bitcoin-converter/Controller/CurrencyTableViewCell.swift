958eb1c2857ce798f323fcfb635e15d641ca3fd4
felipeweber <weberecomp@gmail.com>
2020-06-04 15:52:54 -0300

    Pequenos ajustes

---
+++ bitcoin-converter/Controller/CurrencyTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CurrencyTableViewCell.swift
+//  bitcoin-converter
+//
+//  Created by Felipe Weber on 02/06/20.
+//  Copyright © 2020 Felipe Weber. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var currencyLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+}
